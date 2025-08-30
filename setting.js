d32c95a16326387c2ab172ae469c34ea31737e71
st119848 <40877288+st119848@users.noreply.github.com>
2025-08-30 12:14:23 +0700

    ปรับค่าทั้งหมดเป็นตัวแปร

---
+++ setting.js
@@ -0,0 +1,14 @@
+// Define the settings for the course
+const settings = {
+    currentState: "quickBird", // Possible values: earlyBird, quickBird, lateBird
+    earlyBirdPrice: 4900,
+    quickBirdPrice: 6900,
+    lateBirdPrice: 9900,
+    packageWorkPrice: 10000, // Additional price for guaranteed work package
+    packageTAPrice: 5000, // Additional price for teaching assistant package
+    packageFullPrice: 10000, // Additional price for packageFull
+    earlyBirdEndDate: "23/08/2568", // End date for Early Bird pricing
+    quickBirdEndDate: "31/08/2568", // End date for Quick Bird pricing
+    applyEndDate: "05/09/2568", // Application deadline
+    courseStartDate: "06/09/2568" // Course start date
+};
